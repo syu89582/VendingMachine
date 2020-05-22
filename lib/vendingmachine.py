af8bf35116686036f0e1bfe6069b0e122ddaa7a4
yehching0109 <30792655+yehching0109@users.noreply.github.com>
2020-05-22 22:06:47 +0800

    update show_item and fix str/int bugs

--- lib/vendingmachine.py
+++ lib/vendingmachine.py
@@ -1,10 +1,22 @@
-
 class VendingMachine():
     def __init__(self, items=[]):
         self.items = items
         self.coin = [100, 50, 10, 5, 1]
         self.payment = 0
+    #    
+    def show_items(self, price=999, showall=0):
+        if showall == 1:
+            print("No\tItem\tPrice\tAmount\t\n")
+            for item in self.items:
+                print('%s %10s %3d  %8s' % (item["itemID"], item["itemName"], item["price"], int(item["amount"]))
+        else:
+            print("No\tItem\tPrice\t\n")
+            for item in self.items:
+                if price >= int(item['price']) and int(item["amount"]) > 0:
+                    print('%s %10s %s' % (item["itemID"], item["itemName"], item["price"]))
 
+    #         
+    
     def refund(self):
         refund_coin = {}
         for c in self.coin:
@@ -14,7 +26,7 @@ class VendingMachine():
                     refund_coin[c] += 1
                 else:
                     refund_coin[c] = 1
-
+        self.payment = 0
         return refund_coin
 
     def insert_coin(self, coin):
@@ -29,10 +41,9 @@ class VendingMachine():
     def buy(self, itemID):
         for item in self.items:
             if item["itemID"] == itemID:
-                if self.payment >= item["price"]:
-                    item["amount"] -= 1
-                    self.payment -= item["price"]
-                    self.refund()
+                if self.payment >= int(item["price"]):
+                    int(item["amount"]) -= 1
+                    self.payment -= int(item["price"])
                     return 1
 
         return 0
